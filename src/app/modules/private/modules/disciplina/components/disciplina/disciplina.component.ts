9d6523e1305ce931f514dc1abe3600cbb7d2c023
Carlos Henrique <carlos.has.h284@gmail.com>
2020-06-10 23:11:12 -0300

    Ajuste de cores e adicao de Toastr

--- src/app/modules/private/modules/disciplina/components/disciplina/disciplina.component.ts
+++ src/app/modules/private/modules/disciplina/components/disciplina/disciplina.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-disciplina',
   templateUrl: './disciplina.component.html',
   styleUrls: ['./disciplina.component.scss']
 })
-export class DisciplinaComponent implements OnInit {
+export class DisciplinaComponent {
 
-  constructor() { }
+  constructor(private toastr: ToastrService) {}
 
-  ngOnInit(): void {
+  compartilhadoSucesso() {
+    this.toastr.success('Compartilhado com sucesso!');
   }
 
 }
